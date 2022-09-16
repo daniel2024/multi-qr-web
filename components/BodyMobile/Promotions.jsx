df6ef2b3b5e30e14bb970f962060a04b0ca37ecc
daniel2024 <daniel.huanca@modo.com.ar>
2022-09-16 15:41:29 -0300

    add menu and reserva

--- components/BodyMobile/Promotions.jsx
+++ components/BodyMobile/Promotions.jsx
@@ -40,12 +40,23 @@ overflow-x: scroll;
 padding: 0px 10px;
 width: 100%;
 height: 116px;
+`;
 
-
+const ContainerImage = styled.div`
+background-color: #f2f2f2;
 `;
 
 
 const Promotions = ({ goToPage }) => {
+
+    const handleOpenMenu = () => {
+        window.open("https://www.starbucks.com.ar/menu","__blank");
+    }
+
+    const handleOpenReservas = () => {
+        window.open("https://pedicafe.com.ar/search/type/pickup/address/-34.5465691,-58.4587003","__blank");
+    }
+
   return (
     <ContainerPromotions>
       <div
@@ -55,18 +66,16 @@ const Promotions = ({ goToPage }) => {
         <Image alt="logo" src="/MODO-logo.svg" width={48} height={11} />
       </div>
       <ContainerScroll>
-        
-          <img alt="promo1" src="/promo1.svg"   />
-          <img alt="promo2" src="/promo2.svg"  />
-        
+          <img alt="promo1" src="/promo1.svg"/>
+          <img alt="promo2" src="/promo2.svg"/>
       </ContainerScroll>
       <ContainerButtons>
         <Button
           label={'Metodo De Pago'}
           onClick={() => goToPage(SCREEN_KEY_MOBILE.PAYMENT_METHODS)}
         />
-        <Button label={'Carta'} onClick={() => {}} />
-        <Button label={'Reservas'} onClick={() => {}} />
+        <Button label={'Carta'} onClick={handleOpenMenu}/>
+        <Button label={'Reservas'} onClick={handleOpenReservas}/>
       </ContainerButtons>
     </ContainerPromotions>
   );
